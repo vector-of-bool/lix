98cc4870684b4d1abdc2b656881d65bac8e2e054
vector-of-bool <vectorofbool@gmail.com>
2018-02-05 00:42:12 -0700

    Rename to_value visitor

--- source/let/exec/stack.cpp
+++ source/let/exec/stack.cpp
@@ -22,18 +22,19 @@ let::list exec::convert_ex(const ex_list& list) {
 
 namespace {
 
-struct value_convert_visitor {
-    value operator()(const value& val) { return val; }
-    value operator()(const ex_tuple& tup) { return convert_ex(tup); }
-    value operator()(const ex_list& list) { return convert_ex(list); }
-    value operator()(exec::binding_slot) {
+struct to_value_impl {
+    value to_value(const value& val) { return val; }
+    value to_value(const ex_tuple& tup) { return convert_ex(tup); }
+    value to_value(const ex_list& list) { return convert_ex(list); }
+    value to_value(exec::binding_slot) {
         throw std::runtime_error{"Cannot convert binding slot to value"};
     }
-    value operator()(const exec::closure& cl) {
-        return cl;
-    }
+    value to_value(const ex_cons&) { throw std::runtime_error{"Cannot convert cons to value"}; }
+    value to_value(const exec::closure& cl) { return cl; }
 };
 
 }  // namespace
 
-value stack_element::convert_to_value() const { return visit(value_convert_visitor{}); }
+value stack_element::convert_to_value() const {
+    return visit([](const auto& what) -> value { return to_value_impl{}.to_value(what); });
+}
